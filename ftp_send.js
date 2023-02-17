438b3a680646b9510d306583a38f8af620208e29
YoSuhWOF <110609997+KansasCityWX@users.noreply.github.com>
2023-02-17 17:01:50 -0600

    Update ftp_send.js

--- ftp_send.js
+++ ftp_send.js
@@ -1,1 +1,27 @@
+const ftp = require("basic-ftp");
+const fs = require("fs");
 
+async function sendFile() {
+  const client = new ftp.Client();
+  client.ftp.verbose = true;
+
+  try {
+    await client.access({
+      host: "ftp.example.com",
+      user: "your-username",
+      password: "your-password",
+      secure: false,
+    });
+
+    const fileContents = await fs.promises.readFile("path/to/your/file.txt");
+    await client.uploadFrom(fileContents, "file.txt");
+
+    console.log("File uploaded successfully!");
+  } catch (err) {
+    console.error(err);
+  }
+
+  client.close();
+}
+
+sendFile();
